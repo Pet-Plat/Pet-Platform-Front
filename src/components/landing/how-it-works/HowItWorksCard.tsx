bff4bebdb2fc7f1dd2401d8ee8a7bdd1753ddba0
Vladislav Artiukhov <vladislav0art.work@gmail.com>
2024-07-29 10:24:25 +0200

    feat: Implement How-It-Works section (work in progress)

--- src/components/landing/how-it-works/HowItWorksCard.tsx
+++ src/components/landing/how-it-works/HowItWorksCard.tsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { cn } from "@/lib/utils.ts";
 
-type T = { title: string; description: string }
+type T = { title: string; description: string, className: string }
 
-export default function HowItWorksCard({ title, description }: T) {
+export default function HowItWorksCard({ className, title, description }: T) {
     return (
-        <div className="p-4 border-white border-2 rounded-2xl min-w-60 bg-darkblue">
-            <h4 className="text-lg font-bold mb-2">{title}</h4>
-            <p className="text-sm">{description}</p>
+        <div className={cn("p-4 border-white border-2 rounded-2xl min-h-48 min-w-60 bg-darkblue", className)}>
+            <h4 className="text-lg font-bold mb-2 md:text-2xl md:mb-4">{title}</h4>
+            <p className="text-sm md:text-base">{description}</p>
         </div>
     )
 }
--- src/components/landing/how-it-works/HowItWorksSection.tsx
+++ src/components/landing/how-it-works/HowItWorksSection.tsx
@@ -1,35 +1,44 @@
-import { cn } from "@/lib/utils";
 import React from "react";
 import HowItWorksCard from "@/components/landing/how-it-works/HowItWorksCard.tsx";
 
 
-const cards = [
-    {
-        id: 1,
-        title: "Поиск проектов",
-        description: "Исследуйте наш каталог проектов и выберите тот, который вам интересен.",
-    },
-    {
-        id: 2,
-        title: "Подача заявки",
-        description: "Подайте заявку на участие в одном или нескольких проектах, которые вас заинтересовали.",
-    },
-    {
-        id: 3,
-        title: "title 3",
-        description: "Lorem Ipsum",
-    }
-];
-
+// TODO: set type
 export default function HowItWorksSection() {
     return (
-        <div className={cn("")}>
+        <div className="mb-36">
             <h2 className="container mb-8">Как это работает</h2>
 
-            <div className={cn("grid grid-flow-col gap-4 overflow-auto md:container")}>
-                {cards.map(card => (
-                    <HowItWorksCard key={card.id} title={card.title} description={card.description} />
-                ))}
+            <div className="overflow-auto flex gap-4 px-5 md:container md:gap-6 md:flex-col">
+                <div className="flex gap-4 md:gap-6 md:flex-1">
+                    <HowItWorksCard
+                        className="md:flex-4"
+                        title="Поиск проектов"
+                        description="Исследуйте наш каталог проектов и выберите тот, который вам интересен."
+                    />
+                    <HowItWorksCard
+                        className="md:flex-2 lg:min-w-72"
+                        title="Подача заявки"
+                        description="Подайте заявку на участие в одном или нескольких проектах, которые вас заинтересовали."
+                    />
+                    <HowItWorksCard
+                        className="md:flex-5"
+                        title="Создание команды"
+                        description="Мы подберём единомышленников, и вы начнёте работу над проектом. Не забывайте, что вы можете пригласить своих друзей в команду!"
+                    />
+                </div>
+
+                <div className="flex flex-row gap-4 md:gap-6 md:flex-1">
+                    <HowItWorksCard
+                        className="flex-1"
+                        title="Получите менторскую поддержку"
+                        description="К вашей команде будет прикреплен наставник, который поможет организовать работу и будет направять вас."
+                    />
+                    <HowItWorksCard
+                        className="flex-1"
+                        title="Завершите проект"
+                        description="Совершенствуйте себя, завершайте проект, получайте отзывы отментора и платформы, добавляйте проект в своё портфолио иполучайте предложения о работе или аутсорсе."
+                    />
+                </div>
             </div>
         </div>
     )
